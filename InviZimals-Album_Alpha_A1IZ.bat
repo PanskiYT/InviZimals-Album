c60a0713e0d0cc00439460a9d418b36754c8aae8
Cristian Soncco <cristian_soncco@msn.com>
2023-07-03 07:06:23 -0500

    Funkcje i walidacje dotyczące albumu do utworzenia.

--- InviZimals-Album_Alpha_A1IZ.bat
+++ InviZimals-Album_Alpha_A1IZ.bat
@@ -1,15 +1,76 @@
 @echo off
 title InviZimals Album Alpha A1IZ
 mode 120, 40
-:setup
+
+REM Zabarwienie
+set "kolor_tła=0"
+set "kolor_tekstu=7"
+
+REM Funkcja czyszczenia ekranu
+:czysty_ekran
+cls
+color %kolor_tła%%kolor_tekstu%
+
+
+REM Funkcja do konfiguracji
+:konfiguracji
+cls
 echo ########## Album InviZimals ##########
 echo.
 echo Witamy w konfiguracji albumu InviZimals!
 set /p name="Wprowadz nazwe albumu> "
-echo %name% >"album_name.txt"
-cls
+call :wazne_istnienie_albumu
+:uzytkownik
 echo ########## Album InviZimals ##########
 echo.
 echo Teraz wpisz swoj nick
 set /p nick="Nick> "
+echo.
+echo Powitanie %nick%
+
+
+REM Funkcja wyświetlania menu głównego
+:menu_glowne
+echo ########Indeks albumów########
+echo 1. Wymień Invizimals.
+echo 2. Dodaj Invizimals.
+echo 3. Wychodzić
+echo.
 pause
+
+
+:Wychodzić
+echo Do zobaczenia później %nick%. Teraz wystarczy nacisnąć klawisz, aby wyjść.
+pause
+exit
+
+
+
+
+
+
+REM Funkcja sprawdzania poprawności istnienia albumu
+:wazne_istnienie_albumu
+if exist "%name%.txt" (
+   echo "Album już istnieje."
+   call :kolejny_album
+) else (
+    echo "" > "%name%.txt"
+    echo "Utworzono album. Naciśnij dowolny klawisz, aby kontynuować."
+    pause
+    goto  :uzytkownik
+)
+
+
+
+REM Funkcja zapytaj, czy chcesz utworzyć kolejny album
+:kolejny_album
+set /p ponownieUtworzyc="Czy chcesz utworzyć inny?(1:Y/0:N)> "
+if %ponownieUtworzyc% == 1 ( 
+    goto :konfiguracji
+) 
+else 
+(
+    goto :uzytkownik
+)
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # InviZimals-Album
 Komputerowy album inviZimals
+
+Se crearon funciones, validamos existencia del album a crear  petición del usuario,
+preguntamos si desea crear otro album de ser el caso que exista el que intenta crear. Luego de preguntar el nick muestra menu principal.
